a221e37c194640906670bfada2f6ca8678a064b6
kevin Gonzalo  Cruz Martinez <kevingonzalocruzmartinez@gmail.com>
2024-11-26 01:47:56 -0600

    a

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,7 +9,7 @@ import { ConfigModule } from '@nestjs/config';
     ConfigModule.forRoot({
       isGlobal: true,
     }),
-    MongooseModule.forRoot(process.env.MONGODB_URI || 'mongodb://localhost:27017/proyecto-mensaje'),
+    MongooseModule.forRoot(process.env.MONGO_URI),
     UserModule,
     MessageModule,
   ],
